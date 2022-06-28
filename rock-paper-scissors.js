e251d35bf988bf88ea2cb02d49239af774fc8c55
Yeltay Bayandinov <bayandinov13@gmail.com>
2022-06-28 23:09:12 +0600

    Final Commit

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -34,6 +34,46 @@ const playRound = (computerSelection, playerSelection) => {
   }
 };
 
-const playerSelection = "Paper";
-const computerSelection = computerPlay();
-console.log(playRound(computerSelection, playerSelection));
+const game = () => {
+  const games = 6;
+  let playerPoints = 0;
+  let computerPoints = 0;
+
+  for (let i = 0; i < games; i++) {
+    let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+
+    function computerPlay() {
+      return ["paper", "scissors", "rock"][(Math.random() * 3) | 0];
+    }
+    let computerSelection = computerPlay();
+
+    let round = playRound(playerSelection, computerSelection);
+    if (round === "Player Won") {
+      playerPoints++;
+      console.log("You Win");
+    } else if (round === "Computer Won") {
+      computerPoints++;
+      console.log("You Lose");
+    } else if (round === "ties") {
+      console.log("tied");
+    }
+  }
+
+  if (playerPoints > computerPoints) {
+    console.log("");
+    console.log(
+      "Congratulations!  You beat the computer " +
+        playerPoints +
+        " to " +
+        computerPoints
+    );
+  } else {
+    console.log(
+      "Sorry!  You lost to the computer " +
+        computerPoints +
+        " to " +
+        playerPoints
+    );
+  }
+}; // end game();
+console.log(game());
