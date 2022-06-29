4c0abd4e7f07732ecdec9bb2772cdf86c5bb2705
Yeltay Bayandinov <bayandinov13@gmail.com>
2022-06-30 00:53:09 +0600

    Refactor

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,57 +10,41 @@ const computerPlay = () => {
 };
 
 const playRound = (computerSelection, playerSelection) => {
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection === computerSelection) {
-    return "Tie";
-  } else if (playerSelection == "rock") {
-    if (computerSelection == "paper") {
-      return "Computer Won";
-    } else {
-      return "Player Won";
-    }
-  } else if (playerSelection == "scissors") {
-    if (computerSelection == "rock") {
-      return "Computer Won";
-    } else {
-      return "Player Won";
-    }
-  } else if (playerSelection == "paper") {
-    if (computerSelection == "scissors") {
-      return "Computer Won";
-    } else {
-      return "Player Won";
-    }
-  }
-};
-
-const game = () => {
-  const games = 6;
+  const games = 5;
   let playerPoints = 0;
   let computerPoints = 0;
-
   for (let i = 0; i < games; i++) {
-    let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-
-    function computerPlay() {
-      return ["paper", "scissors", "rock"][(Math.random() * 3) | 0];
-    }
-    let computerSelection = computerPlay();
-
-    let round = playRound(playerSelection, computerSelection);
-    if (round === "Player Won") {
-      playerPoints++;
-      console.log("You Win");
-    } else if (round === "Computer Won") {
-      computerPoints++;
-      console.log("You Lose");
-    } else if (round === "ties") {
-      console.log("tied");
+    computerSelection = computerPlay();
+    playerSelection = prompt("rock, paper or scissors?").toLowerCase();
+    if (playerSelection === computerSelection) {
+      console.log("Tie");
+    } else if (playerSelection == "rock") {
+      if (computerSelection == "paper") {
+        computerPoints++;
+        console.log("Computer Won");
+      } else {
+        playerPoints++;
+        console.log("Player Won");
+      }
+    } else if (playerSelection == "scissors") {
+      if (computerSelection == "rock") {
+        computerPoints++;
+        console.log("Computer Won");
+      } else {
+        playerPoints++;
+        console.log("Player Won");
+      }
+    } else if (playerSelection == "paper") {
+      if (computerSelection == "scissors") {
+        computerPoints++;
+        console.log("Computer Won");
+      } else {
+        playerPoints++;
+        console.log("Player Won");
+      }
     }
   }
-
   if (playerPoints > computerPoints) {
-    console.log("");
     console.log(
       "Congratulations!  You beat the computer " +
         playerPoints +
@@ -75,5 +59,5 @@ const game = () => {
         playerPoints
     );
   }
-}; // end game();
-console.log(game());
+};
+console.log(playRound());
